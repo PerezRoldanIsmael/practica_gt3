7186595cbf2400066d9ca9c19392c4986c6cd8fb
PerezRoldanIsmael <ismael.perez.roldan@alumnos.upm.es>
2018-05-18 17:15:58 +0200

    inital commit

---
+++ practica_gt3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="practica_gt3.MvcApplication" Language="C#" %>
---
+++ practica_gt3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(practica_gt3.Startup))]
+namespace practica_gt3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
